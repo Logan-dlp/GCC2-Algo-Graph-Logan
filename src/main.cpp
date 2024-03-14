2fc35f4d3a824ab29a1c1a5aa4e9500c18635241
Logan <pix.59700@gmail.com>
2024-03-14 11:11:24 +0100

    Generate Random Graph methode

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+
+void GenerateRandomGraph(int n, int m, int edges[][2]);
+
 int main() {
+    std::cout << "Practical exercices: connected components" << std::endl;
+
+    int n, m; //Nombre de sommets et d'arêtes
+
+    std::cout << "Entrer le nombre de sommets: ";
+    std::cin >> n;
+    std::cout << "Entrer le nombre d'arêtes: ";
+    std::cin >> m;
+
+    int edges[m][2];
+    int comp[n];
+
     return 0;
+}
+
+void GenerateRandomGraph(int n, int m, int edges[][2]) {
+    for (int i = 0; i < m; ++i) {
+        std::srand(time(nullptr));
+
+        edges[i][0] = std::rand() % n;
+        edges[i][1] = std::rand() % n;
+    }
 }
