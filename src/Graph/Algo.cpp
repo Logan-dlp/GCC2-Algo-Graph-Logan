f2aa08a542a54731b9711acd8d62fb44680b3c13
Logan <pix.59700@gmail.com>
2024-03-14 16:45:17 +0100

    Bug in algorithme for display exercice

--- src/Graph/Algo.cpp
+++ src/Graph/Algo.cpp
@@ -1,5 +1,9 @@
 #include "../../include/Graph/Algo.hpp"
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 namespace graph::algo {
     // V = n = sommet
     // E = m = arete
@@ -10,12 +14,31 @@ namespace graph::algo {
         for (int i = 0; i < m; ++i) {
             if (comp[edges[i][0]] != comp[edges[i][1]]) {
                 int aux = comp[edges[i][0]];
-                for (int j = 0; j < m; ++j) {
+                for (int j = 0; j < n; ++j) {
                     if (comp[j] == aux) {
                         comp[j] = comp[edges[i][1]];
                     }
                 }
             }
+        }
+    }
+
+    void DisplayComponentsSizes(int n, int m, int comp[]) {
+        int isolated_points = 0;
+        std::vector<int> other_sizes(n, 0); // Vecteur pour stocker les tailles des composantes
+
+        for (int i = 0; i <= n; i++) {
+            if (comp[i] == 1)
+                isolated_points++;
+            else
+                other_sizes[comp[i]]++;
+        }
+
+        std::cout << "Nombre de points isoles : " << isolated_points << std::endl;
+        std::cout << "Nombre de composantes par taille (ordre croissant) :" << std::endl;
+        for (int i = 2; i <= n; i++) {
+            if (other_sizes[i] > 0)
+                std::cout << "Taille " << i << " : " << other_sizes[i] << std::endl;
         }
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -26,5 +26,7 @@ int main() {
     GraphAlgo::ComputeRelatedComponents(n, m, edges, comp);
     GraphUtils::DisplayComponentsMatrix(n, comp);
 
+    GraphAlgo::DisplayComponentsSizes(n, m, comp);
+
     return 0;
 }
